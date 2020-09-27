583a30c5a6715b8cf87c0606e633b6ea274874b7
HenrikDK2 <henrik265@outlook.dk>
2020-09-27 18:06:15 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,8 @@ module.exports = {
     alias: {
       react: "preact/compat",
       "react-dom": "preact/compat",
+      "create-react-class": "preact-compat/lib/create-react-class",
+      "react-dom-factories": "preact-compat/lib/react-dom-factories",
     },
   },
   devServer: { historyApiFallback: true },
